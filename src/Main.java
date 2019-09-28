33c8207812060a284dd732dd3f8192a8d83c47a6
jhonsd1996 <jasedanod@unal.edu.co>
2019-09-28 14:38:21 -0500

    Input BufferedReader

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "test.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                //System.out.println(line);
+                String[] arrOfStr = line.split("");
+                //for (String a : arrOfStr)
+                    //System.out.println(a);
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
